ccc3b47fd1209052f4586b9312ee8bdafc9b7fa8
Uma Shankar Bharawa <ubharawa@gmail.com>
2025-07-20 17:25:26 +0530

    Enhance UI of TaskForm, TaskItem, and TaskList components with improved styling and layout

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -24,21 +24,22 @@ export default function TaskForm() {
     }
 
     return (
-        <>
-            <div>
-                <h1>Task Tracker</h1>
-                <p>Welcome to the Task Tracker App!</p>
-                <div>
+        <div className=" bg-gray-900 flex flex-col items-center justify-center min-h-screen">
+            <div className="bg-gray-300 w-full max-w-[60rem] p-10 rounded shadow-lg/20 shadow-gray-300">
+                <h1 className="text-center text-xl text-gray-900">Task Tracker</h1>
+                <p className="text-center text-gray-800">Welcome to the Task Tracker App!</p>
+                <div className="flex items-center justify-center mt-2">
                     <input
                         type="text"
                         placeholder='Enter a task...'
                         value={inputValue}
                         onChange={handleInputChange}
+                        className="border border-gray-400 p-2 rounded mr-2 focus:outline"
                     />
-                    <button onClick={addTask} className=''>Add Task</button>
+                    <button onClick={addTask} className='border-none cursor-pointer bg-blue-600 transform hover:scale-105 hover:bg-blue-800 transition duration-700 px-3 py-2 text-white text-sm rounded shadow-lg'>Add Task</button>
                 </div>
             </div>
             <TaskList tasks={tasks} setTasks={setTasks} />
-        </>
+        </div>
     )
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -8,7 +8,7 @@ export default function TaskList(props) {
     }
 
     return (
-        <div className=''>
+        <div className='border p-5 w-full max-w-[60rem] mt-4 rounded shadow-lg/20 shadow-gray-200 bg-gray-200'>
             <ul>
                 {props.tasks.map((task) => {
                     return (
